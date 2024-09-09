6c3e9c14f1dac97dc616c32726a11e07f9d2601e
Keith Ferney <keith@ferney.org>
2024-09-08 18:05:38 -0600

    level manager

--- code/globals/nav_button.gd
+++ code/globals/nav_button.gd
@@ -3,6 +3,6 @@ extends Button
 @export var scene : PackedScene
 
 func _on_nav_button_pressed():
-	print(scene.get_meta('order'))
+	print(scene.get_meta_list())
 	Nav.set_lvl(scene.get_meta('order') - 1)
 	get_tree().change_scene_to_packed(scene)
