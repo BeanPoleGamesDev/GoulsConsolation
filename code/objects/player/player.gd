c985acc87a7b87115b0141642ce1bf425691c629
jacobabarbary <jacob@ferney.org>
2024-09-08 20:58:01 -0600

    added stuff

--- code/objects/player/player.gd
+++ code/objects/player/player.gd
@@ -7,13 +7,20 @@ const move_distance = 1000
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_up"):
 		velocity.y = -move_distance
+		update_npc()
 	if Input.is_action_just_pressed("ui_down"):
+		update_npc()
 		velocity.y = move_distance
 	if Input.is_action_just_pressed("ui_left"):
+		update_npc()
 		velocity.x = -move_distance
 	if Input.is_action_just_pressed("ui_right"):
+		update_npc()
 		velocity.x = move_distance
 
 	move_and_slide()
 	velocity.y = 0
 	velocity.x = 0
+
+func update_npc():
+	get_tree().call_group("npc","_player_update")
--- npc_navigation_body.gd
+++ npc_navigation_body.gd
@@ -5,6 +5,12 @@ var nav_close_to_point_radius = 0.5
 var current_nav_path = []
 var direction = Vector2()
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@export var follower_or_not = false
+@export var ray_size = 278
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var sprite_2d_2: Sprite2D = $Sprite2D2
+
+var is_dead = false
 
 func _ready() -> void:
 	pass
@@ -13,14 +19,23 @@ func complete_murder():
 	queue_free()
 
 func kill():
+	is_dead = true
 	animation_player.play("kill")
 
 func _physics_process(delta: float) -> void:
-	
+	var player = get_tree().get_nodes_in_group("player")[0]
+	ray_cast_2d.target_position = player.global_position.direction_to(global_position) * ray_size
+	sprite_2d_2.global_position = ray_cast_2d.get_collision_point()
+
+func _player_update():
+	if is_dead:
+		return
 	index_points()
 	
-	if Input.is_action_just_pressed("click"):
-		pathfind_to_mouse()
+	if follower_or_not:
+		pathfind_to_player()
+	else:
+		pathfind_away_from_player()
 
 func is_at_point():
 	if global_position.distance_to(current_nav_path.front()) < nav_close_to_point_radius:
@@ -37,8 +52,18 @@ func index_points():
 
 func move_to_position(new_position:Vector2):
 	global_position = new_position
-	print(velocity)
-
-func pathfind_to_mouse():
 	
-	current_nav_path = tiles.get_navigation_path(global_position,get_global_mouse_position())
+
+func pathfind_to_player():
+	var player = get_tree().get_nodes_in_group("player")[0]
+	
+	current_nav_path = tiles.get_navigation_path(global_position,player.global_position)
+
+func pathfind_away_from_player():
+	var ray_cast_collision_point = ray_cast_2d.get_collision_point()
+	var ray_cast_collision_safe_offset = ray_cast_collision_point.direction_to(global_position) * 20
+	var nav_point = ray_cast_collision_point + ray_cast_collision_safe_offset
+	
+	current_nav_path = tiles.get_navigation_path(global_position,nav_point)
+	print(current_nav_path)
+	
